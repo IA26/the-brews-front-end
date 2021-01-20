7111225eb31b4544810d194c3625114bde73a8b3
Isaac <isaac1180@aol.com>
2021-01-20 14:09:43 -0500

    added mdbreact

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
 
         <h1>Welcome to The Brews</h1> 
         <h2>Here is a selection of alcohol</h2>
+
+        <button>Button here</button> 
       </header>
     </div>
   );
