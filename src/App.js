c6d7586f4cd84dddfcf5c1e06fa8d0d24fe1719a
Isaac <isaac1180@aol.com>
2021-01-25 17:53:15 -0500

    got rid of comments

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <h1>Welcome to The Brews</h1> 
         <h2>Here is a selection of alcohol</h2>
 
-        {/* <button>Button here</button>  */}
+
 
        
       </header>
